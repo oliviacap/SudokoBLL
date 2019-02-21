553d1f6a8bceb5210ee8ff18d3a5d7ef91bb27f3
livcap <livcap@wifi-roaming-128-4-80-128.host.udel.edu>
2019-02-21 13:02:05 -0500

    ya

--- SukdouBLL/src/main/java/pkgHelper/LatinSquare.java
+++ SukdouBLL/src/main/java/pkgHelper/LatinSquare.java
@@ -2,28 +2,34 @@ package pkgHelper;
 
 import java.util.Arrays;
 
-public class LatinSquare {
+public class LatinSquare 
+{
 
 	private int[][] LatinSquare;
 
-	public LatinSquare() {
+	public LatinSquare() 
+	{
 
 	}
 
-	public LatinSquare(int[][] latinSquare) {
+	public LatinSquare(int[][] latinSquare) 
+	{
 		super();
 		LatinSquare = latinSquare;
 	}
 
-	public int[][] getLatinSquare() {
+	public int[][] getLatinSquare() 
+	{
 		return LatinSquare;
 	}
 
-	public void setLatinSquare(int[][] latinSquare) {
+	public void setLatinSquare(int[][] latinSquare)
+	{
 		LatinSquare = latinSquare;
 	}
 
-	public boolean hasDuplicates(int[] arr) {
+	public boolean hasDuplicates(int[] arr)
+	{
 		
 		boolean hasDuplicates = false;
 		if (arr == null)
@@ -31,7 +37,8 @@ public class LatinSquare {
 
 		Arrays.sort(arr); 
 
-		for (int i = 0; i < arr.length - 1; i++) {
+		for (int i = 0; i < arr.length - 1; i++) 
+		{
 			if (arr[i] == arr[i + 1]) {
 				hasDuplicates = true;
 				break;
@@ -40,7 +47,8 @@ public class LatinSquare {
 		}
 		return hasDuplicates;
 	}
-	public boolean doesElementExist(int[] arr, int iValue) {
+	public boolean doesElementExist(int[] arr, int iValue)
+	{
 		
 		boolean doesElementExist = false;
 		
@@ -57,9 +65,74 @@ public class LatinSquare {
 		
 		return doesElementExist;
 		
-		
-		
 	}
+	
+	/**
+	 * Pass in two, one dimensional arrays 
+	 * if everything in arr2 exists in arr1, return true
+	 * 
+	 * @param arr1 - array to check
+	 * @param arr2 - array used to check
+	 * 
+	 * example:
+	 * 
+	 * if arr1 = 1,2,3,4,5 and arr2 = 2,5, return true
+	 * if arr1 = 1,2,3,4,5 and arr2 = 2,6, return false
+	 * @return
+	 */
+	
+	public boolean hasAllValues(int[] arr1, int[] arr2)
+	{
+		return false;
+	}
+	
+	/**
+	 * Return a one dimensional array of a column from Latin square
+	 * @param Col
+	 * @return
+	 */
+	public int[] getColumn(int Col)
+	{
+		return null;
+	}
+	
+	/** 
+	 * Return a one dimensional array of a row from Latin square
+	 * 
+	 * @param Row
+	 * @return
+	 */
+	public int[] getRow(int Row)
+	{
+		
+		return null;
+	}
+	
+	/**
+	 * Return 'true' if...
+	 * 
+	 * Each row in LatinSquare does not contain duplicates
+	 * Each column in LatinSquare does not contain duplicates
+	 * Each value in the first row is found in every other row of the LatinSquare
+	 * Each value in the first row is found in every other column of the LatinSquare
+	 * 
+	 * @return - true or false, see above
+	 */
+	
+	public boolean isLatinSquare() 
+	{
+		
+		return false;
+	}
+	/**
+	 * containsZero - will return 'true' if any value in the LatinSquare is zero.
+	 * @return
+	 */
+	public boolean containsZero()
+	{
+		return false;
+	}
+	
 	
 }
 
